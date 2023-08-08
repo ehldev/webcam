5f30b427679816b7e5828971f20f49323850cbb9
Erick Huamani @ehldev <dev.erick95@gmail.com>
2023-08-08 03:16:22 -0500

    Deploy para pruebas

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Aquí se mostrará la transmisión de mi webcam -->
+    <div id="video-container">
+      <video ref="videoElement" autoplay></video>
+    </div>
+
+    <button @click="startWebcam">Mostrar webcam</button>
+    <button @click="capturePhoto" :disabled="!videoStream">Tomar foto</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      videoStream: null
+    }
+  },
+  methods: {
+    async startWebcam() {
+      try {
+        this.videoStream = await navigator.mediaDevices.getUserMedia({ video: true });
+        this.$refs.videoElement.srcObject = this.videoStream;
+      } catch (error) {
+        console.error('Error al acceder a la cámara:', error);
+      }
+    },
+    capturePhoto() {
+      const canvas = document.createElement('canvas');
+      canvas.width = this.$refs.videoElement.videoWidth;
+      canvas.height = this.$refs.videoElement.videoHeight;
+      const context = canvas.getContext('2d');
+      context.drawImage(this.$refs.videoElement, 0, 0, canvas.width, canvas.height);
+
+      canvas.toBlob(blob => {
+        if (blob) {
+          const url = URL.createObjectURL(blob);
+
+          const link = document.createElement('a');
+          link.href = url;
+          link.download = 'captured_photo.png';
+          link.click();
+
+          URL.revokeObjectURL(url);
+        }
+      }, 'image/png');
+    }
+  },
+  beforeDestroy() {
+    if (this.videoStream) {
+      this.videoStream.getTracks().forEach(track => track.stop());
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#video-container {
+  margin: 20px;
 }
 </style>
